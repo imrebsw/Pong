3bc467d39cd927a37112ede2308113d3f617d652
rebekah <33032837+imrebsw@users.noreply.github.com>
2019-04-19 21:42:37 -0700

    Update Paddle.java

--- src/unit15/Paddle.java
+++ src/unit15/Paddle.java
@@ -16,52 +16,57 @@ public class Paddle extends Block {
         speed = 5;
     }
 
-    public Paddle(int x, int y) {
-        super(x, y);
-        speed = 1;
+    public Paddle(int xPos, int yPos) {
+        super(xPos, yPos);
+        this.speed = 5;
     }
 
-    public Paddle(int x, int y, int w) {
-        super(x, y, w);
-        speed = 3;
+    public Paddle(int xPos, int yPos, int speed) {
+        super(xPos, yPos);
+        this.speed = speed;
     }
 
-    public Paddle(int x, int y, int w, int h) {
-        super(x, y, w, h);
-        speed = 3;
+    public Paddle(int xPos, int yPos, int width, int height, int speed) {
+        super(xPos, yPos, width, height);
+        this.speed = speed;
     }
 
-    public Paddle(int x, int y, int w, int h, int xSp) {
-        super(x, y, w, h);
-        speed = xSp;
-    }
-
-    public Paddle(int x, int y, int w, int h, Color c, int xSp) {
-        super(x, y, w, h, c);
-        speed = xSp;
+    public Paddle(int xPos, int yPos, int width, int height, Color color, int speed) {
+        super(xPos, yPos, width, height, color);
+        this.speed = speed;
     }
 
     //add the other Paddle constructors
-    public void setSpeed(int a) {
-        speed = a;
-    }
-
-    public int getSpeed() {
-        return 50;
-    }
-
     public void moveUpAndDraw(Graphics window) {
-        draw(window, Color.white);
-        setxPos(getxPos() + getSpeed());
+        window.setColor(Color.white);
+        window.fillRect(super.getxPos(),super.getyPos(),super.getWidth(),super.getHeight());
+        setyPos(getyPos() - getSpeed());
+        //draw(window,getColor());
+        window.setColor(super.getColor());
+        window.fillRect(super.getxPos(),super.getyPos(),super.getWidth(),super.getHeight());
     }
 
     public void moveDownAndDraw(Graphics window) {
-
+        window.setColor(Color.white);
+        window.fillRect(super.getxPos(),super.getyPos(),super.getWidth(),super.getHeight());
+        setyPos(getyPos() + getSpeed());
+        //draw(window,getColor());
+        window.setColor(super.getColor());
+        window.fillRect(super.getxPos(),super.getyPos(),super.getWidth(),super.getHeight());
     }
 
-    public String toString() {
-        return getxPos() + "" + getyPos() + "" + getWidth() + "" + getHeight() + "" + getColor() + "" + getSpeed();
-    }
    //add get methods
     //add a toString() method
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return "" + getxPos() + " " + getyPos() + " " + getWidth() + " " + getHeight() + " " + getColor() + " " + getSpeed();
+    }
 }
